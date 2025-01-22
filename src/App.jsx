7d52470d41c5b068d151998af6f05f02920fd2b1
unknown <snehamariyakj123@gmail.com>
2025-01-22 15:50:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>sneha</h1>
+      <input type="text"placehold="username" />
+      <br/>
+      <button>lig in</button>
+    </>
+  )
+}
+
+export default App
+
